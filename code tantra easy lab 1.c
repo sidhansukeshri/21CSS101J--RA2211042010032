12a438bd86f364ed7bf9c59580d2e6c96e3f8fe0
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-14 11:14:17 +0530

    Update and rename float to double.c to code tantra easy lab 1.c

---
+++ code tantra easy lab 1.c
@@ -0,0 +1,15 @@
+/*On one beautiful Sunday Selvan went to Aaron's house for exam preparation. 
+
+They have decided to study Mathematics subject because they have exams by coming Monday, Aaron is a master in Mathematics but Selvan is not so good in Mathematics so James trained with Selvan for getting a high score in the exam. 
+
+After teaching some problems to Selvan.Aaron have given some tasks to Selvan to solve .
+
+The problem is to convert input float into a double. Can you help Selvan in finding the solution ? */
+#include<stdio.h>
+//my reg no. RA2211042010032
+main(){
+float a,b;
+scanf("%f %f",&a,&b);
+printf("%lf\n%1f\n",a,b);
+return 0;
+}
--- float to double.c
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-//my reg no. RA2211042010032
-main(){
-float a,b;
-scanf("%f %f",&a,&b);
-printf("%lf\n%1f\n",a,b);
-return 0;
-}
