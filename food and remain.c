1e384711d9f431156eaa16f4decd6bd1afd8279c
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-14 10:57:58 +0530

    Create food and remain.c

---
+++ food and remain.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+// my Reg no.RA2211042010032
+int main(){
+int avl,mess, food, remain;
+scanf("%d %d",&avl,&mess); 
+food=avl/mess;
+remain=av1%mess;
+printf("%d %d", food, remain);
+}
