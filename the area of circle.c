4faa75aceb4717f5f7425a05286c3f5394187e0a
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-23 22:03:59 +0530

    Create the area of circle.c

---
+++ the area of circle.c
@@ -0,0 +1,11 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+main()
+{  
+  printf("RA2211042010042\n");
+  float r;
+  printf("enter radius ");
+  scanf("%f",&r);
+  float area=3.14*r*r;
+  printf("The area=%f",area);
+}
