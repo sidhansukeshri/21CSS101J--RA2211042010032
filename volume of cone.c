71bc394cbb1a47ffb4ad2251ab7fcecd08a53e71
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-23 22:07:37 +0530

    Create volume of cone.c

---
+++ volume of cone.c
@@ -0,0 +1,11 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+main()
+{ 
+  float radius;
+ float height;
+  printf("enter radius of cone");
+  scanf("%f",&radius);
+  float volume=0.33*3.14*radius*radius*radius;
+  printf("The volume=%f",volume);
+}
