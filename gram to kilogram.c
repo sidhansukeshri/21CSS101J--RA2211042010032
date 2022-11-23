7a9e07d7f499656fcb54a177cdf889f676fc39d7
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-23 22:16:17 +0530

    Create gram to kilogram.c

---
+++ gram to kilogram.c
@@ -0,0 +1,11 @@
+//my reg no is RA2211042010032
+#include<stdio.h>
+int main()
+{   
+    float kg, g;
+    printf("Enter weight in gram: ");
+    scanf("%f", &g);
+    kg = g/1000;
+    printf("Equivalent weight in kilogram = %f", kg);
+    return 0;
+}
