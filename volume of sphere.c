298bd3489fb8b8946d74e3bc8243d90e38c74725
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-23 22:09:19 +0530

    Create volume of sphere.c

---
+++ volume of sphere.c
@@ -0,0 +1,10 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+main()
+{
+  float radius;
+  printf("enter radius of sphere");
+  scanf("%f",&radius);
+  float volume=1.33*3.14*radius*radius*radius;
+  printf("The volume=%f",volume);
+}
