d43dfb878efbf15590cf277b5d755a9bd3c47f55
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-23 22:13:33 +0530

    Create centimeter to meter.c

---
+++ centimeter to meter.c
@@ -0,0 +1,10 @@
+//my reg no is RA2211042010032
+#include<stdio.h>
+int main()
+{   
+    float cm, m;
+    printf("Enter length in cm: ");
+    scanf("%f", &cm);
+    m= cm/100;
+    printf("Equivalent length in m = %fm", m);
+}
