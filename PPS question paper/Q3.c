98d49309101f0609fd5c07a3808dcb6a757683ba
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-25 01:50:20 +0530

    Create Q3.c

---
+++ PPS question paper/Q3.c
@@ -0,0 +1,12 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+int main()
+{
+    int N, M;
+    printf("Number of friends: ");
+    scanf("%d", &N);
+    printf("Number of left shoes: ");
+    scanf("%d", &M);
+    printf("Number of extra pairs of shoes to be bought: %d\n", N-M);
+    printf("Number of right shoes to be bought: %d\n", M);
+}
