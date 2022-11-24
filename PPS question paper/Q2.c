55419da67ce3666a2ef354d9d13bb565e8ef2597
sidhansukeshri <112798263+sidhansukeshri@users.noreply.github.com>
2022-11-25 01:49:02 +0530

    Create Q2.c

---
+++ PPS question paper/Q2.c
@@ -0,0 +1,16 @@
+//my reg no is RA2211042010032
+#include <stdio.h>
+int main()
+{
+    int X,Y;
+    scanf("%d %d", &X, &Y);
+
+    if(Y<=X)
+    {
+        printf("%d\n", Y);
+    }
+    else
+    {
+        printf("%d\n", X);
+    }
+}
